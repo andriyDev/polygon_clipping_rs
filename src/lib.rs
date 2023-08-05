ea1402ee008120837f622f26674ddbb0b79d5089
andriyDev <andriydzikh@gmail.com>
2023-08-05 08:06:46 -0700

    Create basic project setup and outline for operations.

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use glam::Vec2;
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Polygon {
+  pub contours: Vec<Vec<Vec2>>,
+}
+
+pub fn intersection(subject: &Polygon, clip: &Polygon) -> Polygon {
+  perform_boolean(subject, clip, Operation::Intersection)
+}
+
+pub fn union(subject: &Polygon, clip: &Polygon) -> Polygon {
+  perform_boolean(subject, clip, Operation::Union)
+}
+
+pub fn difference(subject: &Polygon, clip: &Polygon) -> Polygon {
+  perform_boolean(subject, clip, Operation::Difference)
+}
+
+pub fn xor(subject: &Polygon, clip: &Polygon) -> Polygon {
+  perform_boolean(subject, clip, Operation::XOR)
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Operation {
+  Intersection,
+  Union,
+  XOR,
+  Difference,
+}
+
+fn perform_boolean(
+  _subject: &Polygon,
+  _clip: &Polygon,
+  _operation: Operation,
+) -> Polygon {
+  todo!()
+}
